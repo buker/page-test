17936419acc177cd1a7a5b5aed407cb335ae82a3
Łukasz Żułnowski <lzulnowski@gmail.com>
2023-11-21 17:25:13 +0100

    first commit

---
+++ src/handler/rest/post/post.go
@@ -0,0 +1,37 @@
+package post
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/buker/page-test/internal/dbsql/user"
+	"github.com/labstack/echo/v4"
+)
+
+func Posts(c echo.Context) error {
+	// Bind the request body to a `User` struct.
+	userr := new(user.Users)
+	if err := c.Bind(userr); err != nil {
+		return err
+	}
+
+	err := userr.Exists()
+	if err != nil {
+		return err
+	} else {
+		err = userr.Create()
+		if err != nil {
+			return err
+		}
+		// Marshal the `userr` struct to JSON.
+		jsonData, err := json.Marshal(userr)
+		if err != nil {
+			return err
+		}
+
+		// Write the JSON data to the response.
+		c.Response().Header().Set("Content-Type", "application/json")
+		return c.JSON(http.StatusOK, jsonData)
+	}
+
+}
---
+++ src/routes/routes.go
@@ -0,0 +1,74 @@
+package routes
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/buker/page-test/internal/appdata"
+	"github.com/buker/page-test/src/handler/get/home"
+	"github.com/buker/page-test/src/handler/get/loginemail"
+	"github.com/buker/page-test/src/handler/post/createuser"
+	"github.com/buker/page-test/src/handler/restful/post"
+	"github.com/labstack/echo/v4"
+	//imports
+)
+
+type Stats struct {
+	Exe  string
+	Pid  string
+	Ppid string
+}
+
+type Data struct {
+	Proto   string
+	Host    string
+	Address string
+	Method  string
+	URL     string
+	Path    string
+	Routes  []string
+	Stat    appdata.Stats
+}
+
+func Routes(e *echo.Echo) {
+
+	e.GET("/home", home.Home)
+	e.GET("/loginemail/:email/:sitetoken", loginemail.LoginEmail)
+	//routes
+
+	//post
+	e.POST("/usercreate", createuser.Createuser)
+	e.POST("/p", post.Posts)
+
+	e.GET("/request", func(c echo.Context) error {
+		req := c.Request()
+		ss := e.Routes()
+		var datas []string
+		for key, _ := range ss {
+			data, err := json.MarshalIndent(ss[key], "", "")
+			if err != nil {
+				return err
+			}
+			datas = append(datas, string(data))
+
+		}
+		st, err := appdata.GetAppData()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		d := Data{Proto: req.Proto, Host: req.Host, Address: req.RemoteAddr, Method: req.Method, Path: req.URL.Path, Routes: datas, Stat: st}
+		//needed for nounce to be added to asset links for security to ensure they are those assets loading from this server
+		nonce := c.Get("n")
+		jsr := c.Get("jsr")
+		cssr := c.Get("cssr")
+
+		return c.Render(http.StatusOK, "request.html", map[string]interface{}{
+			"data":  d,
+			"nonce": nonce,
+			"jsr":   jsr,
+			"cssr":  cssr,
+		})
+	})
+}
